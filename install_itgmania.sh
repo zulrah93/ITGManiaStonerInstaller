7d20a858d6d14d2967c1d6cd7bfedb9329946210
zulrah93 <37640543+zulrah93@users.noreply.github.com>
2023-09-01 14:35:07 -0400

    Add install_itgmania.sh

---
+++ install_itgmania.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+read -p "Please read disclaimer that this installer may do harm to your system; do you want to proceed? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+    echo "You chose the safe route"
+then
+    echo "I warned you! Downloading setup files and installing dependencies via apt-get"
+    wget https://github.com/itgmania/itgmania/releases/download/v0.7.0/ITGmania-0.7.0-Linux-no-songs.tar.gz
+    tar xzf ITGmania-0.7.0-Linux-no-songs.tar.gz
+    cd ITGmania-0.7.0-Linux-no-songs/
+    sudo ./setup.sh
+    sudo apt-get install libusb-0.1-4
+    echo "To launch  ~/ITGmania-0.7.0-Linux-no-songs/itgmania/itgmania"
+    echo "It finished pray it works and good luck on your fantastic or marvelous journey!"
+fi
+
